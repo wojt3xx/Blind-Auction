a15c006f885b585a1290bb6d218e50d3baed8539
v01dxx <117851354+v01dxx@users.noreply.github.com>
2022-11-11 23:25:00 +0200

    Added code

---
+++ blind_auction.py
@@ -0,0 +1,31 @@
+import art
+
+print(art.logo)
+print("Welcome to the secret auction program.")
+
+bidders_dictionary = [
+
+]
+
+
+def add_bidders(my_name, my_bid):
+    bidders_dictionary.append({"name": str(my_name), "bid": int(my_bid)})
+
+
+name = input("What is your name?: ")
+bid = input("What is your bid?: $")
+add_bidders(name, bid)
+
+more_bidders = True
+
+while more_bidders:
+    other_bidders = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+
+    if other_bidders == "yes":
+        another_name = input("What is your name?: ")
+        another_bid = input("What is your bid?: $")
+        add_bidders(another_name, another_bid)
+        print(bidders_dictionary)
+    else:
+        print("Shut down")
+        exit()
